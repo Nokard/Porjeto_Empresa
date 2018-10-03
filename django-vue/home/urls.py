8ba4149cbd3cc7c36eed8db970646a56e4586ed5
rodrigueshugodias <rodrigueshugodias@hotmail.com>
2018-10-03 17:42:59 -0300

    Passando a url

--- django-vue/home/urls.py
+++ django-vue/home/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 from django.urls import path
 from home.views import index
-from .views import listarCnpjView
+from . import views 
 
 
 app_name = 'home'
 urlpatterns = [
     path('', index, name="index"),
     #path('/home/empresas', empresas, name="empresas"),
-    path('CNPJ/<int:pk>/', listarCnpjView.as_view(), name="lista_cnpj"),
+    path('CNPJ/<int:pk>/', views.listarCnpjView.as_view(), name="lista_cnpj"),
+    path('', views.EmpresasList.as_view()),
 
 ]
--- django-vue/home/views.py
+++ django-vue/home/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.serializers import serialize
 from django.http import HttpResponse
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
 
 from .models import Empresas
+
+
+class EmpresasList(ListView):
+    model = Empresas
 
 
 def index(request):
@@ -15,7 +20,7 @@ def index(request):
     else:
         Emp = Empresas.objects.all()[:10]
         
-    return render(request,'home/index.html',{'Empresas': Emp })
+    return render(request,'home/empresas_list.html',{'Empresas': Emp })
 
 
 class listarCnpjView(DetailView):
